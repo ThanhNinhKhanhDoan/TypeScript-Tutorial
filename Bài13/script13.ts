c4bb175ff599c60d2b02f6fec2eb387b94ca3fc5
NTNDev <thanhninh05102001@gmail.com>
2024-06-12 19:48:57 +0700

    Hoc xong bài 13 ve kieu tuple

--- Bài13/script13.ts
+++ Bài13/script13.ts
@@ -1,1 +1,15 @@
-let skills: [string, number] = ["KD", 23]
+let skills: (string | number)[] = ["KD", 23];
+skills.push("DT");
+skills.push(24);
+
+console.log(">>>check skills: ", skills)
+
+// Kiểu tuple: dataType/size/order
+
+let kieu_tuple: [string, number] = ["TN", 23];
+
+let kieu_tuple2: [boolean, string, number?]; // dấu hỏi đó là để khi ta tạo kiểu nhưng không bắt buộ khai báo giá trị
+kieu_tuple2 = [true, "KDTN", 23]
+
+console.log(">>>check kieu_tuple: ", kieu_tuple);
+console.log(">>>check kieu_tuple2: ", kieu_tuple2);
